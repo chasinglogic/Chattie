a4e1b5caa2d91d0322ff78be91007922ec69f91f
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-04-26 19:00:55 -0400

    Added a yoda translation command

---
+++ commands/yoda.py
@@ -0,0 +1,12 @@
+import requests
+
+# http://api.funtranslations.com/translate/yoda.json?text=
+def translate(msg):
+    r = requests.get("http://api.funtranslations.com/translate/yoda.json?text=" + msg)
+    try:
+        return r.json()['contents']['translated']
+    except:
+        return "An unexpected error occured"
+
+def run(thorin, incoming):
+    return translate(incoming.message.text)
