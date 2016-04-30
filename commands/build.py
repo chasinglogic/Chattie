f06fa638a21322053be3ce8d092812a7d7cfc7ec
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-04-30 14:13:13 -0400

    should be working? need to set up a test build to test this

--- commands/build.py
+++ commands/build.py
@@ -5,8 +5,12 @@ JENKINS_URL = getenv("THORIN_JENKINS_URL")
 JENKINS_TOKEN = getenv("THORIN_JENKINS_TOKEN")
 
 def run_build(strarr):
-    build = strarr[strarr.index("build") + 1]
-    r = requests.post(JENKINS_URL + "job/" + build + "/build?token=" + JEKNINS_TOKEN) 
+    r = requests.post(JENKINS_URL + "/job/" + strarr[strarr.index("build") + 1] + "/build?token=" + JEKNINS_TOKEN) 
+    if r.status_code > 300:
+        print("Error with the request:", r.text)
+        return "I couldn't start the build."
+    else:
+        return "Build successfully started."
 
 def run(thorin, incoming):
     split = incoming.message.text.split(" ")
