3959e6511147cdc3e4a965e183a768174ffedc59
Ryan Brzezinski <ryan.brzezinski867@gmail.com>
2017-05-02 15:54:58 -0400

    move weather into default tricks

--- src/thorin/tricks/__init__.py
+++ src/thorin/tricks/__init__.py
@@ -2,7 +2,6 @@
 
 import requests
 import random
-import thorin.tricks.weather as weather
 
 from os import getenv
 from inspect import getdoc
@@ -79,6 +78,55 @@ def build(bot, inc_msg):
         return "Build successfully started."
 
 
+WEATHER_API_KEY = getenv("THORIN_WEATHER_API_TOKEN")
+
+
+def _is_zip_code(requested):
+    try:
+        int(requested)
+        return True
+    except:
+        return False
+
+
+def _format_resp(jsn):
+    return 'Temperature: %s Condition: %s'.format(
+        jsn['main']['temp'],
+        jsn['weather'][0]['description']
+    )
+
+
+# We make a call to:
+# api.openweathermap.org/data/2.5/weather?q={city name}
+def _get_weather_name(city):
+    r = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" +
+                     city + "&APPID=" + WEATHER_API_KEY + "&units=imperial")
+    if r.status_code == 404:
+        return "I can't find that city"
+    if r.status_code > 300:
+        return "Some unknown error occurred"
+    return _format_resp(r.json())
+
+
+# api.openweathermap.org/data/2.5/weather?zip={zip code}
+def _get_weather_zip(zipcode):
+    r = requests.get("http://api.openweathermap.org/data/2.5/weather?zip="
+                     + zipcode + "&APPID=" + WEATHER_API_KEY + "&units=imperial")
+    if r.status_code == 404:
+        return "I can't find that city"
+    if r.status_code > 300:
+        return "Some unknown error occurred"
+    return _format_resp(r.json())
+
+
+def weather(thorin, incoming):
+    """Get the weather for a given city name or zip code."""
+    city_or_zip = str(incoming[incoming.index("weather"):])
+    if _is_zip_code(city_or_zip):
+        return _get_weather_zip(city_or_zip)
+    return _get_weather_name(city_or_zip)
+
+
 commands = {
     'help': helpcmd,
     'pick': pick,
@@ -87,8 +135,8 @@ commands = {
     'hello': hello
 }
 
-# if weather.API_KEY:
-#     commands['weather'] = weather.weather
+if WEATHER_API_KEY:
+    commands['weather'] = weather.weather
 
 if JENKINS_API_KEY:
     commands['build'] = build
