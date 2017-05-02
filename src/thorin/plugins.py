df6ac8c63c4de35d066a3b5527f69ab346b98601
Mathew Robinson <chasinglogic@gmail.com>
2017-05-02 14:03:19 -0400

    add code for loading plugins

---
+++ src/thorin/plugins.py
@@ -0,0 +1,15 @@
+"""Contains methods for finding commands and connectors."""
+
+import pkg_resources
+
+
+def get_connectors():
+    """Find all connectors available on the system."""
+    return [v for v in
+            pkg_resources.iter_entry_points('thorin.plugins.connectors')]
+
+
+def get_commands():
+    """Find all commands available on the system."""
+    return [v for v in
+            pkg_resources.iter_entry_points('thorin.plugins.tricks')]
