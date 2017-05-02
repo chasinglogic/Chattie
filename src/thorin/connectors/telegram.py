313baebc2fcab6c67736078e1cde84eb3ca3e903
Ryan Brzezinski <ryan.brzezinski867@gmail.com>
2017-05-02 15:54:34 -0400

    update telegram connector to latest sdk version

--- src/thorin/connectors/telegram.py
+++ src/thorin/connectors/telegram.py
@@ -17,30 +17,21 @@ class Connector(object):
         token = os.getenv('TELEGRAM_API_TOKEN')
         if token is None:
             raise Exception('TELEGRAM_API_TOKEN not set')
-        self.updater = Updater(token)
+        self.updater = Updater(token=token)
         self.dispatcher = self.updater.dispatcher
         self.parser = parser
-        self.dispatch.addHandler(MessageHandler([], self.parse_incoming))
+        self.dispatcher.addHandler(MessageHandler([], self.parse_incoming))
 
     def listen(self):
         """Listen for messages."""
         self.updater.start_polling()
         self.updater.idle()
 
-    def connect(self):
-        """Connect to the rooms specified by TELEGRAM_ROOMS.
-
-        TELEGRAM_ROOMS can be a comma seperated list of room names.
-        """
-        rooms = os.getenv('TELEGRAM_ROOMS')
-        if rooms is None:
-            raise Exception('TELEGRAM_ROOMS not set')
-
     def send_message(self, room_id, msg):
         """Send a message to the given room_id."""
-        self.bots[room_id].sendMessage(room_id, text=msg)
+        self.bots[str(room_id)].sendMessage(chat_id=room_id, text=msg)
 
     def parse_incoming(self, bot, incoming):
         """Transform incoming telegram info into format Thorin can parse."""
+        self.bots[str(incoming.message.chat_id)] = bot
         self.parser(incoming.message.chat_id, incoming.message.text)
-        self.bots[incoming.message.chat_id] = bot
